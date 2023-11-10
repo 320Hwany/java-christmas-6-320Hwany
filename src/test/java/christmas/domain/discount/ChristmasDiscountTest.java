c68bea597de9b2647321e228ed26ef37019466dc
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-10 18:43:20 +0900

    test : 크리스마스 디데이 할인 적용

---
+++ src/test/java/christmas/domain/discount/ChristmasDiscountTest.java
@@ -0,0 +1,33 @@
+package christmas.domain.discount;
+
+import christmas.domain.Menu;
+import christmas.domain.Order;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ChristmasDiscountTest {
+
+    @DisplayName("크리스마스 디데이 할인을 적용하고 할인 금액을 계산한다.")
+    @Test
+    void applyDiscount() {
+        // given
+        String menuName1 = "양송이수프";
+        String menuName2 = "초코케이크";
+        Menu menu1 = Menu.createMenu(menuName1, 5);
+        Menu menu2 = Menu.createMenu(menuName2, 5);
+        List<Menu> menus = List.of(menu1, menu2);
+        Order order = new Order(menus, 10);
+
+        ChristmasDiscount christmasDiscount = new ChristmasDiscount();
+
+        // when
+        int discountPrice = christmasDiscount.applyDiscount(order);
+
+        // then
+        assertThat(discountPrice).isEqualTo(-1900);
+    }
+}
