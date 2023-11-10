b727e7cd6d41c031b9591066884b6069be8b9456
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-10 09:53:12 +0900

    test : 문자열로 작성된 숫자를 정수형으로 변환

---
+++ src/test/java/christmas/view/valid/ViewValidatorTest.java
@@ -0,0 +1,35 @@
+package christmas.view.valid;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ViewValidatorTest {
+
+    @DisplayName("문자열로 작성된 숫자를 정수 타입으로 변환한다.")
+    @Test
+    void parseIntSuccess() {
+        // given
+        ViewValidator viewValidator = new ViewValidator();
+        String inputText = "10";
+
+        // when
+        int expectedVisitDate = viewValidator.parseInt(inputText);
+
+        // then
+        assertThat(expectedVisitDate).isEqualTo(10);
+    }
+
+    @DisplayName("문자열로 작성된 숫자를 정수 타입으로 변환할 수 없으며 예외가 발생한다.")
+    @Test
+    void parseIntFail() {
+        // given
+        ViewValidator viewValidator = new ViewValidator();
+        String inputText = "숫자가 아님";
+
+        // expected
+        assertThatThrownBy(() -> viewValidator.parseInt(inputText))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
