754c2d84fc4eee72c630bd7ef9355b9fb3e449a7
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-12 19:40:25 +0900

    test(MenusTest) : 메뉴 목록 총금액, 할인 계산

--- src/test/java/christmas/domain/menu/MenusTest.java
+++ src/test/java/christmas/domain/menu/MenusTest.java
@@ -1,10 +1,14 @@
 package christmas.domain.menu;
 
+import christmas.constant.PriceConstant;
+import christmas.domain.date.ExpectedVisitDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static christmas.constant.PriceConstant.WEEKDAY_DISCOUNT_UNIT;
+import static christmas.constant.PriceConstant.WEEKEND_DISCOUNT_UNIT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -66,5 +70,64 @@ class MenusTest {
 
         // then
         assertThat(menus).isNotNull();
+    }
+
+    @DisplayName("메뉴 목록에서 메뉴들의 총 금액을 계산한다.")
+    @Test
+    void calculateTotalPrice() {
+        // given
+        String menuName1 = "양송이수프";
+        String menuName2 = "초코케이크";
+        Menu menu1 = Menu.createMenu(menuName1, 5);
+        Menu menu2 = Menu.createMenu(menuName2, 3);
+        Menus menus = new Menus(List.of(menu1, menu2));
+
+        // when
+        int totalPrice = menus.calculateTotalPrice();
+
+        // then
+        assertThat(totalPrice).isEqualTo(6000 * 5 + 15000 * 3);
+    }
+
+    @DisplayName("메뉴 목록에서 평일 할인을 적용하고 할인 값을 반환한다.")
+    @Test
+    void calculateTotalWeekdayDiscount() {
+        // given 1
+        String menuName1 = "티본스테이크";
+        String menuName2 = "초코케이크";
+        Menu menu1 = Menu.createMenu(menuName1, 5);
+        Menu menu2 = Menu.createMenu(menuName2, 3);
+        Menus menus = new Menus(List.of(menu1, menu2));
+
+        // given 2
+        int weekday = 3;
+        ExpectedVisitDate expectedVisitDate = new ExpectedVisitDate(weekday);
+
+        // when
+        int totalWeekdayDiscount = menus.calculateTotalWeekdayDiscount(expectedVisitDate);
+
+        // then
+        assertThat(totalWeekdayDiscount).isEqualTo(-WEEKDAY_DISCOUNT_UNIT.price * 3);
+    }
+
+    @DisplayName("메뉴 목록에서 주말 할인을 적용하고 할인 값을 반환한다.")
+    @Test
+    void calculateWeekendDiscount() {
+        // given 1
+        String menuName1 = "티본스테이크";
+        String menuName2 = "초코케이크";
+        Menu menu1 = Menu.createMenu(menuName1, 5);
+        Menu menu2 = Menu.createMenu(menuName2, 3);
+        Menus menus = new Menus(List.of(menu1, menu2));
+
+        // given 2
+        int weekend = 2;
+        ExpectedVisitDate expectedVisitDate = new ExpectedVisitDate(weekend);
+
+        // when
+        int totalWeekdayDiscount = menus.calculateTotalWeekendDiscount(expectedVisitDate);
+
+        // then
+        assertThat(totalWeekdayDiscount).isEqualTo(-WEEKEND_DISCOUNT_UNIT.price * 5);
     }
 }
