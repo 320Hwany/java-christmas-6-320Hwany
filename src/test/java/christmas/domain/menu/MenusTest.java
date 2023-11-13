de2cf1a1273920bc689e57315c197e312dce5b93
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-13 14:33:09 +0900

    refactor(MenusTest) : ParameterizedTest 사용하도록 개선

--- src/test/java/christmas/domain/menu/MenusTest.java
+++ src/test/java/christmas/domain/menu/MenusTest.java
@@ -1,9 +1,9 @@
 package christmas.domain.menu;
 
-import christmas.constant.PriceConstant;
 import christmas.domain.date.ExpectedVisitDate;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -15,11 +15,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class MenusTest {
 
     @DisplayName("주문한 메뉴 개수의 총합이 20개를 초과하면 예외가 발생한다.")
-    @Test
-    void validateMenusQuantity() {
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 초코케이크"})
+    void validateMenusQuantity(final String menuName1, final String menuName2) {
         // given
-        String menuName1 = "양송이수프";
-        String menuName2 = "초코케이크";
         Menu menu1 = Menu.createMenu(menuName1, 10);
         Menu menu2 = Menu.createMenu(menuName2, 11);
 
@@ -29,13 +28,12 @@ class MenusTest {
     }
 
     @DisplayName("주문한 메뉴 중에서 중복된 메뉴가 있으면 예외가 발생한다.")
-    @Test
-    void validateDuplication() {
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 양송이수프"})
+    void validateDuplication(final String duplicationMenuName1, final String duplicationMenuName2) {
         // given
-        String menuName1 = "양송이수프";
-        String menuName2 = "양송이수프";
-        Menu menu1 = Menu.createMenu(menuName1, 10);
-        Menu menu2 = Menu.createMenu(menuName2, 10);
+        Menu menu1 = Menu.createMenu(duplicationMenuName1, 10);
+        Menu menu2 = Menu.createMenu(duplicationMenuName2, 10);
 
         // expected
         assertThatThrownBy(() -> Menus.from(List.of(menu1, menu2)))
@@ -43,13 +41,12 @@ class MenusTest {
     }
 
     @DisplayName("주문한 메뉴 중에서 음료만 있으면 예외가 발생한다.")
-    @Test
-    void validateOnlyBeverage() {
+    @ParameterizedTest
+    @CsvSource({"레드와인, 제로콜라"})
+    void validateOnlyBeverage(final String beverageMenuName1, final String beverageMenuName2) {
         // given
-        String menuName1 = "레드와인";
-        String menuName2 = "제로콜라";
-        Menu menu1 = Menu.createMenu(menuName1, 10);
-        Menu menu2 = Menu.createMenu(menuName2, 10);
+        Menu menu1 = Menu.createMenu(beverageMenuName1, 10);
+        Menu menu2 = Menu.createMenu(beverageMenuName2, 10);
 
         // expected
         assertThatThrownBy(() -> Menus.from(List.of(menu1, menu2)))
@@ -57,11 +54,10 @@ class MenusTest {
     }
 
     @DisplayName("최대 개수/중복/음료만 주문과 같은 조건에 제한되지 않으면 메뉴 목록이 생성된다.")
-    @Test
-    void createMenus() {
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 초코케이크"})
+    void createMenus(final String menuName1, final String menuName2) {
         // given
-        String menuName1 = "양송이수프";
-        String menuName2 = "초코케이크";
         Menu menu1 = Menu.createMenu(menuName1, 10);
         Menu menu2 = Menu.createMenu(menuName2, 10);
 
@@ -73,11 +69,10 @@ class MenusTest {
     }
 
     @DisplayName("메뉴 목록에서 메뉴들의 총 금액을 계산한다.")
-    @Test
-    void calculateTotalPrice() {
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 초코케이크"})
+    void calculateTotalPrice(final String menuName1, final String menuName2) {
         // given
-        String menuName1 = "양송이수프";
-        String menuName2 = "초코케이크";
         Menu menu1 = Menu.createMenu(menuName1, 5);
         Menu menu2 = Menu.createMenu(menuName2, 3);
         Menus menus = Menus.from(List.of(menu1, menu2));
@@ -90,17 +85,16 @@ class MenusTest {
     }
 
     @DisplayName("메뉴 목록에서 평일 할인을 적용하고 할인 값을 반환한다.")
-    @Test
-    void calculateTotalWeekdayDiscount() {
+    @ParameterizedTest
+    @CsvSource({"티본스테이크, 초코케이크, 3"})
+    void calculateTotalWeekdayDiscount(final String mainMenuName, final String dessertMenuName,
+                                       final int weekday) {
         // given 1
-        String menuName1 = "티본스테이크";
-        String menuName2 = "초코케이크";
-        Menu menu1 = Menu.createMenu(menuName1, 5);
-        Menu menu2 = Menu.createMenu(menuName2, 3);
-        Menus menus = Menus.from(List.of(menu1, menu2));
+        Menu mainMenu = Menu.createMenu(mainMenuName, 5);
+        Menu dessertMenu = Menu.createMenu(dessertMenuName, 3);
+        Menus menus = Menus.from(List.of(mainMenu, dessertMenu));
 
         // given 2
-        int weekday = 3;
         ExpectedVisitDate expectedVisitDate = ExpectedVisitDate.from(weekday);
 
         // when
@@ -111,17 +105,16 @@ class MenusTest {
     }
 
     @DisplayName("메뉴 목록에서 주말 할인을 적용하고 할인 값을 반환한다.")
-    @Test
-    void calculateWeekendDiscount() {
+    @ParameterizedTest
+    @CsvSource({"티본스테이크, 초코케이크, 2"})
+    void calculateWeekendDiscount(final String mainMenuName, final String dessertMenuName,
+                                  final int weekend) {
         // given 1
-        String menuName1 = "티본스테이크";
-        String menuName2 = "초코케이크";
-        Menu menu1 = Menu.createMenu(menuName1, 5);
-        Menu menu2 = Menu.createMenu(menuName2, 3);
-        Menus menus = Menus.from(List.of(menu1, menu2));
+        Menu mainMenu = Menu.createMenu(mainMenuName, 5);
+        Menu dessertMenu = Menu.createMenu(dessertMenuName, 3);
+        Menus menus = Menus.from(List.of(mainMenu, dessertMenu));
 
         // given 2
-        int weekend = 2;
         ExpectedVisitDate expectedVisitDate = ExpectedVisitDate.from(weekend);
 
         // when
