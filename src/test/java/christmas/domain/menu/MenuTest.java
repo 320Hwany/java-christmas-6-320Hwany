19ce7b52106a89743389197958e81d78c0ee4c6c
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-13 14:40:19 +0900

    refactor(MenuTest) : ParameterizedTest 사용하도록 개선

--- src/test/java/christmas/domain/menu/MenuTest.java
+++ src/test/java/christmas/domain/menu/MenuTest.java
@@ -2,7 +2,8 @@ package christmas.domain.menu;
 
 import christmas.domain.date.ExpectedVisitDate;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static christmas.constant.PriceConstant.WEEKDAY_DISCOUNT_UNIT;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,36 +12,31 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class MenuTest {
 
     @DisplayName("메뉴 개수는 1이상의 숫자이면 메뉴를 생성한다.")
-    @Test
-    void validateQuantity() {
-        // given
-        String menuName = "양송이수프";
-        int quantity = 1;
-
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 1"})
+    void validateQuantity(final String menuName, final int validQuantity) {
         // when
-        Menu menu = Menu.createMenu(menuName, quantity);
+        Menu menu = Menu.createMenu(menuName, validQuantity);
 
         // expected
         assertThat(menu).isNotNull();
     }
 
     @DisplayName("메뉴 개수는 1이상의 숫자가 아니면 예외가 발생한다.")
-    @Test
-    void validateQuantityFail() {
-        // given
-        String menuName = "양송이수프";
-        int quantity = 0;
-
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 0"})
+    void validateQuantityFail(final String menuName, final int invalidQuantity) {
         // expected
-        assertThatThrownBy(() -> Menu.createMenu(menuName, quantity))
+        assertThatThrownBy(() -> Menu.createMenu(menuName, invalidQuantity))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("메뉴 수량과 메뉴 가격으로부터 한 가지 메뉴의 가격을 계산한다.")
-    @Test
-    void calculatePrice() {
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 5"})
+    void calculatePrice(final String menuName, final int validQuantity) {
         // given
-        Menu menu = Menu.createMenu("양송이수프", 5);
+        Menu menu = Menu.createMenu(menuName, validQuantity);
 
         // when
         int price = menu.calculatePrice();
@@ -50,12 +46,12 @@ class MenuTest {
     }
 
     @DisplayName("평일 할인 금액을 계산하고 값을 반환한다.")
-    @Test
-    void calculateWeekdayDiscount() {
+    @ParameterizedTest
+    @CsvSource({"초코케이크, 5, 3, 2"})
+    void calculateWeekdayDiscount(final String dessertMenuName, final int validQuantity,
+                                  final int weekday, final int weekend) {
         // given
-        Menu menu = Menu.createMenu("초코케이크", 5);
-        int weekday = 3;
-        int weekend = 2;
+        Menu menu = Menu.createMenu(dessertMenuName, validQuantity);
         ExpectedVisitDate expectedVisitDate1 = ExpectedVisitDate.from(weekday);
         ExpectedVisitDate expectedVisitDate2 = ExpectedVisitDate.from(weekend);
 
@@ -69,11 +65,13 @@ class MenuTest {
     }
 
     @DisplayName("메뉴의 종류가 음료인지 아닌지 확인한다.")
-    @Test
-    void isNotBeverageMenu() {
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 제로콜라, 5"})
+    void isNotBeverageMenu(final String notBeverageMenuName, final String beverageMenuName,
+                           final int validQuantity) {
         // given
-        Menu menu1 = Menu.createMenu("양송이수프", 5);
-        Menu menu2 = Menu.createMenu("제로콜라", 5);
+        Menu menu1 = Menu.createMenu(notBeverageMenuName, validQuantity);
+        Menu menu2 = Menu.createMenu(beverageMenuName, validQuantity);
 
         // when
         boolean isNotBeverageMenu1 = menu1.isNotBeverageMenu();
@@ -85,16 +83,16 @@ class MenuTest {
     }
 
     @DisplayName("최종 메뉴의 개수에서 해당 메뉴의 개수를 추가하여 반환한다.")
-    @Test
-    void addQuantity() {
+    @ParameterizedTest
+    @CsvSource({"양송이수프, 5, 0"})
+    void addQuantity(final String menuName, final int validQuantity, final int quantity) {
         // given
-        Menu menu = Menu.createMenu("양송이수프", 5);
-        int totalQuantity = 0;
+        Menu menu = Menu.createMenu(menuName, validQuantity);
 
         // when
-        int quantity = menu.addQuantity(totalQuantity);
+        int totalQuantity = menu.addQuantity(quantity);
 
         // then
-        assertThat(quantity).isEqualTo(5);
+        assertThat(totalQuantity).isEqualTo(5);
     }
 }
