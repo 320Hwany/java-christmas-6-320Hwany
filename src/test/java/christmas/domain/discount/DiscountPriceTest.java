36a4283e499319cb716b192dfc93538700ee95c4
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-13 12:22:17 +0900

    test(DiscountPriceTest) : 증정 이벤트 적용

--- src/test/java/christmas/domain/discount/DiscountPriceTest.java
+++ src/test/java/christmas/domain/discount/DiscountPriceTest.java
@@ -1,4 +1,56 @@
-import static org.junit.jupiter.api.Assertions.*;
+package christmas.domain.discount;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static christmas.constant.DiscountInfoConstant.GIVEAWAY_INDEX;
+import static christmas.constant.PriceConstant.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class DiscountPriceTest {
-  
+
+    @DisplayName("12만원 이상이면 증정 이벤트 적용 대상이다.")
+    @ParameterizedTest
+    @CsvSource({"120000, -1000, 0, -2023, -1000"})
+    void applyGiveawayEvent(final int totalPrice, final int christmasDiscount, final int weekdayDiscount,
+                            final int weekendDiscount, final int specialDiscount) {
+        // given
+        List<Integer> discountPrices = new ArrayList<>();
+        discountPrices.add(christmasDiscount);
+        discountPrices.add(weekdayDiscount);
+        discountPrices.add(weekendDiscount);
+        discountPrices.add(specialDiscount);
+
+        // when
+        DiscountPrice discountPrice = new DiscountPrice(totalPrice, discountPrices);
+        int giveawayPrice = discountPrices.get(GIVEAWAY_INDEX.value);
+
+        // then
+        assertThat(giveawayPrice).isEqualTo(GIVEAWAY_BENEFIT.price);
+    }
+
+    @DisplayName("12만원 미만이면 증정 이벤트 적용 대상이 아니다.")
+    @ParameterizedTest
+    @CsvSource({"119999, -1000, 0, -2023, -1000"})
+    void notApplyGiveawayEvent(final int totalPrice, final int christmasDiscount, final int weekdayDiscount,
+                            final int weekendDiscount, final int specialDiscount) {
+        // given
+        List<Integer> discountPrices = new ArrayList<>();
+        discountPrices.add(christmasDiscount);
+        discountPrices.add(weekdayDiscount);
+        discountPrices.add(weekendDiscount);
+        discountPrices.add(specialDiscount);
+
+        // when
+        DiscountPrice discountPrice = new DiscountPrice(totalPrice, discountPrices);
+        int giveawayPrice = discountPrices.get(GIVEAWAY_INDEX.value);
+
+        // then
+        assertThat(giveawayPrice).isEqualTo(0);
+    }
 }
